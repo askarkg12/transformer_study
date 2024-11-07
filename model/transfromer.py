842e93b85bef886123283c6e81c425781f6654f1
Askar Sulaimanov <askarkg12@gmail.com>
2024-11-07 19:11:32 +0000

    Start flexible transformer

--- model/transfromer.py
+++ model/transfromer.py
@@ -2,9 +2,27 @@ import torch
 import torch.nn as nn
 
 
-class TransformerEncoderLayer(nn.Module):
-    def __init__(self, embedding_dim, n_heads, feedforward_dim) -> None:
+class FlexibleEncoderLayer(nn.Module):
+    def __init__(
+        self,
+        embedding_dim,
+        n_heads,
+        feedforward_dim,
+        residual_attn: bool,
+        norm_attn: bool,
+        residual_ff: bool,
+        norm_ff: bool,
+        residual_both: bool,
+        norm_both: bool,
+    ) -> None:
         super().__init__()
+
+        self.residual_attn = residual_attn
+        self.norm_attn = norm_attn
+        self.residual_ff = residual_ff
+        self.norm_ff = norm_ff
+        self.residual_both = residual_both
+        self.norm_both = norm_both
 
         if embedding_dim % n_heads != 0:
             raise ValueError(
@@ -29,8 +47,22 @@ class TransformerEncoderLayer(nn.Module):
             nn.Linear(feedforward_dim, embedding_dim),
         )
 
+        self.norm = nn.LayerNorm(embedding_dim)
+
     def forward(self, x) -> torch.Tensor:
         # x: [seq_len, embedding_dim]
+        residual_pre_attn = x
+        x = self.self_attention(x)
+        if self.residual_attn:
+            x = x + residual_pre_attn
+        if self.norm_attn:
+            x = self.norm(x)
+
+        residual_post_attn = x
+
+        x = self.feedforward(x)
+
+    def self_attention(self, x):
         heads = []
         for Q, K, V in zip(self.Qs, self.Ks, self.Vs):
             # [seq_len, head_dim]
@@ -43,6 +75,11 @@ class TransformerEncoderLayer(nn.Module):
             attention = torch.softmax(attention, dim=-1)
 
             # [seq_len, head_dim]
-            head = torch.einsum("sd,st->td", Vx, attention)
+            head = torch.einsum("st,td->sd", attention, Vx)
             heads.append(head)
-        pass
+
+        # [seq_len, n_heads * head_dim]
+        heads = torch.cat(heads, dim=-1)
+        # [seq_len, embedding_dim]
+        x = self.projection(heads)
+        return x
