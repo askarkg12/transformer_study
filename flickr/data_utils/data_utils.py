fefe4fbae7873f530735b9d2e4014aaa3203a3d5
Askar Sulaimanov <askarkg12@gmail.com>
2024-11-15 18:01:18 +0000

    Add split_size var

--- flickr/data_utils/data_utils.py
+++ flickr/data_utils/data_utils.py
@@ -16,14 +16,31 @@ val_ds = ds.filter(lambda x: x["split"] == "val")
 
 
 class FlickrDataset(Dataset):
-    def __init__(self, spm_model_path, split: str = "train", width: int = 480):
+    def __init__(
+        self,
+        spm_model_path,
+        split: str = "train",
+        split_size: int | float = None,
+        width: int = 480,
+    ):
         self.tokenizer = spm.SentencePieceProcessor(model_file=spm_model_path)
         # Padding token is outside of vocab
         self.pad_id = self.tokenizer.vocab_size()
         self.img_to_tensor = T.ToTensor()
+
+        if isinstance(split_size, float):
+            if split_size > 1 or split_size < 0:
+                raise ValueError("split_size should be between 0 and 1, if float")
+            ratio = split_size
         hg_ds = load_dataset("nlphuji/flickr30k", split="test").filter(
             lambda x: x["split"] == split
         )
+
+        if isinstance(split_size, int):
+            ratio = split_size / len(hg_ds)
+        if split_size is not None:
+            # Fake random sampling of subset
+            hg_ds = hg_ds.train_test_split(test_size=ratio)["test"]
         self.id_to_img = {
             int(row["img_id"]): self.resize_to_width(row["image"], width)
             for row in tqdm(hg_ds, desc="Preprocessing images")
@@ -66,7 +83,7 @@ class FlickrDataset(Dataset):
     def collate_fn(self, batch):
         imgs, in_caption, out_caption = zip(*batch)
 
-        img_lens = [img.size(0) for img in imgs]
+        img_lens = torch.tensor([img.size(0) for img in imgs])
         img_tnsr = pad_sequence(imgs, batch_first=True)
 
         # No need to calculate twice
@@ -78,7 +95,9 @@ class FlickrDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset = FlickrDataset(spm_model_path="./tokeniser/tknz_20000.model", split="val")
+    dataset = FlickrDataset(
+        spm_model_path="./tokeniser/tknz_20000.model", split="val", split_size=10
+    )
     from torch.utils.data import DataLoader
 
     dl = DataLoader(dataset, batch_size=2, collate_fn=dataset.collate_fn)
